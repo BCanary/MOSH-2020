a97ea3bb152d735f4d161a1ea33c8d9f9b767af3
BCanary <59798021+BCanary@users.noreply.github.com>
2020-02-04 15:46:59 +0300

    Параллелограмм
    Почему то не работает, 20 баллов из 100 3 задания из 11, там и там в 8 заданиях просто ошибка... Плохая тестирующая система?

--- MOSH_PARALELOGRAMM.py
+++ MOSH_PARALELOGRAMM.py
@@ -1,25 +1,20 @@
-from math import sqrt
-def length(x1,y1, x2, y2):
-    p1 = (x1-x2)
-    p2 = (y1-y2)
-    return sqrt(p1*p1 + p2*p2)
-
+def mod(num):
+    if num < 0:
+        return num * -1
+    return num
 N = int(input())
-
 vivod = []
+k = 0.001
 for _ in range(0, N):
-    f = input().split(" ");
-    f = [int(x) for x in f]
-    a = length(f[0], f[1], f[2], f[3])
-    b = length(f[4], f[5], f[6], f[7])
-    c = length(f[0], f[1], f[4], f[5])
-    d = length(f[2], f[3], f[6], f[7])
-
-    if((c/b) == (c/a)):
-        vivod.append("YES");
+    ax,ay,bx,by,cx,cy,dx,dy = input().split(" ")
+    ax = int(ax); ay = int(ay);
+    cx = int(cx); cy = int(cy);
+    bx = int(bx); by = int(by);
+    dx = int(dx); dy = int(dy);
+    if ( ((ax==bx)and(cx==dx)) or ((ax==cx)and(bx==dx)) or ((ax==dx)and(bx==cx)) or ((mod((ay - by)/(ax-bx)-(cy-dy)/(cx-dx)))<k) or ((mod((ay - cy)/(ax-cx)-(by-dy)/(bx-dx)))<k) or ((mod((ay - dy)/(ax-dx)-(by-cy)/(bx-cx)))<k)):
+        vivod.append("YES")
     else:
-        vivod.append("NO");
-
+        vivod.append("NO")
 for i in vivod:
     print(i)
     
---
+++ parma.pas
@@ -0,0 +1,24 @@
+﻿program dsa;
+type point=record  //тип точка
+           x,y:real;
+           end;
+const t=0.001;//точность сравнения вещественных чисел
+var a,b,c,d:point;
+var N, i: integer;
+var arva: array[1..10] of string;
+begin
+readln(N);
+for i:=1 to N do begin
+readln(a.x,a.y,b.x,b.y,c.x,c.y,d.x,d.y);
+//если прямые попарно вертикальны
+if(((a.x=b.x)and(c.x=d.x)) or((a.x=c.x)and(b.x=d.x)) or((a.x=d.x)and(b.x=c.x))
+//или угловые коэффициенты попарно равны
+or((abs((a.y-b.y)/(a.x-b.x)-(c.y-d.y)/(c.x-d.x))<t))
+or((abs((a.y-c.y)/(a.x-c.x)-(b.y-d.y)/(b.x-d.x))<t))
+or((abs((a.y-d.y)/(a.x-d.x)-(b.y-c.y)/(b.x-c.x))<t)))
+then arva[i] := 'YES'
+else arva[i] := 'NO'
+end;
+
+for i := 1 to N do writeln(arva[i]);
+end.
